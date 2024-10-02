aec7ea29051be68645cf2c561eaffa210f571cbe
Abel <vandijkabel@gmail.com>
2024-10-02 14:00:13 +0200

    std button

---
+++ components/UI/Button/Standard.vue
@@ -0,0 +1,31 @@
+﻿<script setup lang="ts">
+
+let props = defineProps(
+    {
+      content: {
+        type: String,
+        required: false
+      }
+    })
+</script>
+
+<template>
+  <button class="button">
+    {{content}}
+  </button>
+</template>
+
+<style scoped>
+
+.button {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+
+</style>
