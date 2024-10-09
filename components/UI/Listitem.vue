97a9d85bb744d40e1fa3249d878ea447644815f7
Cage03 <timmermansstijn1@gmail.com>
2024-10-09 11:18:48 +0200

    Created listItem

--- components/UI/Listitem.vue
+++ components/UI/Listitem.vue
@@ -1,9 +1,33 @@
 ﻿<template>
-
+  <div class="list-item">
+    <template v-for="property in properties">
+      <p>{{property.value}}</p>
+    </template>
+    <UIIcon :icon="IconType.Edit" @click="redirectTo()"/>
+  </div>
 </template>
 
 <script setup lang="ts">
+import {IconType} from "~/types/global-types";
 
+let props = defineProps({
+  properties: {
+    type: Array as () => Array<{ key: string; value: any }>,
+    required: true
+  },
+  redirect: {
+    type: String,
+    required: false,
+  }
+});
+
+const redirectTo = () => {
+  const router = useRouter();
+
+  if (props.redirect) {
+    router.push(props.redirect);
+  }
+};
 </script>
 
 <style scoped lang="scss">
