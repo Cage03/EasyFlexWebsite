781ffb130d93d0d4437700fae05099c466447891
Cage03 <timmermansstijn1@gmail.com>
2024-11-06 09:38:37 +0100

    Ensured phone number input may only be numerical

--- pages/flexworker/get.vue
+++ pages/flexworker/get.vue
@@ -14,7 +14,7 @@ interface flexworkerContent {
 }
 
 const api = useRuntimeConfig().public.apiUrl;
-const responseMap = ref<flexworkerContent>({ id: '', adress: '', name: '', dateOfBirth: '', email: '', phoneNumber: '', profilePictureURL: '' });
+const responseMap = ref<flexworkerContent>({ id: '', adress: '', name: '', dateOfBirth: '', email: '', phoneNumber: '', profilePictureUrl: '' });
 const error = ref(null);
 
 // get id from query params
@@ -31,10 +31,18 @@ const showConfirmationPopup = ref(false);
 const popupMessage = ref('');
 const popupButtonText = ref('Close')
 const phoneNumberRegex = ref("^\\+?[0-9]{1,3}[ \\-]?\\(?[0-9]{1,4}\\)?[ \\-]?[0-9]{3}[ \\-]?[0-9]{3,4}$");
+
 const togglePopup = () => {
   showPopup.value = !showPopup.value;
   if (shouldRedirect.value) {
     router.push('/flexworker');
+  }
+};
+
+const restrictToNumbers = (event: KeyboardEvent) => {
+  const allowedKeys = ["Backspace", "ArrowLeft", "ArrowRight", "Delete", "+", "(", ")", " "];
+  if (!/[0-9]/.test(event.key) && !allowedKeys.includes(event.key)) {
+    event.preventDefault();
   }
 };
 
@@ -50,17 +58,14 @@ const editMode = ref({
   dateOfBirth: false
 });
 
-
 watch(() => responseMap.value, (newValue) => {
   if (newValue && continueWatch) {
     local.value = { ...newValue };
     local.value.dateOfBirth = local.value.dateOfBirth.toString().split('T')[0]
 
     viewableDate = convertToViewAble(new Date(local.value.dateOfBirth));
-    //Prevents props from syncing with the local which is needed to check if a field has changed and not remained the same
     continueWatch = false
   }
-
 });
 
 const handleFieldChange = (field: keyof Content) => {
@@ -74,15 +79,11 @@ function checkIfFieldsChanged() {
   fieldChanged = false;
 
   for (const key in responseMap.value) {
-
     const field = key as keyof flexworkerContent;
-    console.log("i am here " + field);
-
     const localValue = local.value[field];
-
     const propValue = (field === 'dateOfBirth')
-      ? responseMap.value[field].toString().split('T')[0]
-      : responseMap.value[field];
+        ? responseMap.value[field].toString().split('T')[0]
+        : responseMap.value[field];
     if (String(localValue) !== String(propValue)) {
       fieldChanged = true;
       break;
@@ -94,8 +95,8 @@ function convertToViewAble(date) {
   const options = { day: 'numeric', month: 'long', year: 'numeric' };
   return date.toLocaleDateString('en-GB', options);
 }
-async function handleFetch() {
 
+async function handleFetch() {
   continueWatch = true;
   try {
     const res = await fetch(`${api}/Flexworker/GetById?id=${id}`, {
@@ -109,11 +110,9 @@ async function handleFetch() {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
 
-
     const data = await res.json();
     responseMap.value = data;
     router.push({ path: '/flexworker/get', query: { id } });
-
 
   } catch (err: any) {
     error.value = err.message;
@@ -121,144 +120,131 @@ async function handleFetch() {
     router.push('/flexworker/notFound');
   }
 }
+
 const deleteFlexWorker = async () => {
   const confirmed = window.confirm("Are you sure you want to delete this flexworker?");
   if (confirmed) {
     try {
-      console.log("calling delete");
       await useFlexworker.deleteFlexworker(parseInt(<string>id));
       shouldRedirect.value = true;
       popupMessage.value = "Flexworker deleted successfully!";
       showPopup.value = true;
-      console.log('FlexWorker deleted successfully!');
-
     } catch (err: any) {
-      console.log(err);
       shouldRedirect.value = false;
       popupMessage.value = "An error occurred while trying to delete the flexworker";
       showPopup.value = true;
     }
   } else {
-      popupMessage.value = "Job deletion canceled.";
-    }
-  };
+    popupMessage.value = "Job deletion canceled.";
+  }
+};
 
-  async function handleUpdate() {
-    let confirmation = window.confirm("Are you sure you want to save")
-    if (confirmation) {
-      const content = ref<flexworkerContent>({
-        id: id,
-        name: local.value.name,
-        adress: local.value.adress,
-        dateOfBirth: local.value.dateOfBirth,
-        email: local.value.email,
-        phoneNumber: local.value.phoneNumber,
-        profilePictureUrl: local.value.profilePictureUrl
-      })
+async function handleUpdate() {
+  let confirmation = window.confirm("Are you sure you want to save")
+  if (confirmation) {
+    const content = ref<flexworkerContent>({
+      id: id,
+      name: local.value.name,
+      adress: local.value.adress,
+      dateOfBirth: local.value.dateOfBirth,
+      email: local.value.email,
+      phoneNumber: local.value.phoneNumber,
+      profilePictureUrl: local.value.profilePictureUrl
+    });
 
-      const res = await fetch(`${api}/Flexworker/Update`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(content.value)
-      })
+    const res = await fetch(`${api}/Flexworker/Update`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(content.value)
+    })
         .then(response => {
           if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
           } else {
-            handleFetch()
+            handleFetch();
           }
         })
         .then(data => {
-          console.log('Registration successful:', data);
           showSuccessPopup('You have updated successfully!');
           fieldChanged = false;
         })
         .catch(err => {
-          console.error('Registration error:', err);
           showErrorPopup(err.message);
         });
-    }
   }
+}
 
-  
-  const toggleConfirmationPopup = () => {
-    showConfirmationPopup.value = !showConfirmationPopup.value;
-  };
+const toggleConfirmationPopup = () => {
+  showConfirmationPopup.value = !showConfirmationPopup.value;
+};
 
-  function showSuccessPopup(message: string) {
-    togglePopup()
-    popupButtonText.value = 'Close'
-    popupMessage.value = message;
-  }
+function showSuccessPopup(message: string) {
+  togglePopup();
+  popupButtonText.value = 'Close';
+  popupMessage.value = message;
+}
 
-  function showErrorPopup(message: string) {
-    togglePopup()
-    popupButtonText.value = 'Close'
-    popupMessage.value = 'The update was unsuccessful \n' + message;
-  }
+function showErrorPopup(message: string) {
+  togglePopup();
+  popupButtonText.value = 'Close';
+  popupMessage.value = 'The update was unsuccessful \n' + message;
+}
 </script>
 
 <template>
   <UIPopup :show="showPopup" :buttonText="popupButtonText" @close="togglePopup" @xButtonFunction="togglePopup">
-    {{ popupMessage }}</UIPopup>
-  <!--  <UIConfirmationPopUp :show="showConfirmationPopup" :buttonTextCancel="cancel" :buttonTextConfirm="Confirm" @cancel="toggleConfirmationPopup" @confirm="handleUpdate" ></UIConfirmationPopUp>-->
+    {{ popupMessage }}
+  </UIPopup>
 
   <div class="register_page">
     <div class="window">
       <form @submit.prevent="handleUpdate">
         <div class="profile_data_header">
-
           <div class="profile_picture">
-            <img :src="local.profilePictureUrl" alt="Profile Picture" style="    width: 18.75rem; height: 18.75rem" />
+            <img :src="local.profilePictureUrl" alt="Profile Picture" style="width: 18.75rem; height: 18.75rem" />
           </div>
 
           <div class="profile_data">
             <div @click="editMode.name = true" class="editable-field">
               <h1 class="text" v-if="!editMode.name">{{ local.name }}</h1>
               <UIInputField v-else @blur="handleFieldChange('name')" @keydown.enter="handleFieldChange('name')"
-                v-model="local.name" placeholder="Name" style="font-size: 1.5rem" />
+                            v-model="local.name" placeholder="Name" style="font-size: 1.5rem" />
             </div>
 
             <div @click="editMode.adress = true" class="editable-field">
               <p class="text" v-if="!editMode.adress">{{ local.adress }}</p>
               <UIInputField v-else @blur="handleFieldChange('adress')" @keydown.enter="handleFieldChange('adress')"
-                v-model="local.adress" placeholder="Adress" />
+                            v-model="local.adress" placeholder="Adress" />
             </div>
+
             <div @click="editMode.email = true" class="editable-field">
               <p class="text" v-if="!editMode.email">{{ local.email }}</p>
               <UIInputField v-else @blur="handleFieldChange('email')" @keydown.enter="handleFieldChange('email')"
-                v-model="local.email" placeholder="Email" type="email" />
+                            v-model="local.email" placeholder="Email" type="email" />
             </div>
+
             <div @click="editMode.dateOfBirth = true" class="editable-field">
               <p class="text" v-if="!editMode.dateOfBirth">{{ viewableDate }}</p>
               <UIInputField v-else @blur="handleFieldChange('dateOfBirth')"
-                @keydown.enter="handleFieldChange('dateOfBirth')" v-model="local.dateOfBirth"
-                placeholder="Date of Birth" type="date" />
+                            @keydown.enter="handleFieldChange('dateOfBirth')" v-model="local.dateOfBirth"
+                            placeholder="Date of Birth" type="date" />
             </div>
+
             <div @click="editMode.phoneNumber = true" class="editable-field">
               <p class="text" v-if="!editMode.phoneNumber">{{ local.phoneNumber }}</p>
               <UIInputField v-else @blur="handleFieldChange('phoneNumber')"
-                @keydown.enter="handleFieldChange('phoneNumber')" v-model="local.phoneNumber" placeholder="Phone number"
-                type="tel" :pattern=phoneNumberRegex title="Please enter a valid phone number (e.g., +1 234-567-8901)" required/>
+                            @keydown.enter="handleFieldChange('phoneNumber')" @keydown="restrictToNumbers"
+                            v-model="local.phoneNumber" placeholder="Phone number" type="tel" inputmode="numeric"
+                            :pattern="phoneNumberRegex" title="Please enter a valid phone number (e.g., +1 234-567-8901)" required />
             </div>
-            
-            <!--            <UIInputField placeholder="Profile picture url" v-model="profilePictureUrl" type="url" />-->
           </div>
         </div>
 
-        <!--        <div class="features-window">-->
-        <!--          <FeatureBox title="Languages" :features="languages" :newFeature="newLanguage" :addFeature="addLanguage" />-->
-        <!--          <FeatureBox title="Skills" :features="skills" :newFeature="newSkill" :addFeature="addSkill" />-->
-        <!--          <FeatureBox title="Certificates" :features="certificates" :newFeature="newCertificate" :addFeature="addCertificate" />-->
-        <!--        </div>-->
-        <!--        <div class="register-button-container">-->
-        <!--          <UIButtonStandard :content="'Register'" />-->
-        <!--        </div>-->
-        <UIButtonStandard v-if="fieldChanged" :content="'Save changes'"></UIButtonStandard>
-
+        <UIButtonStandard v-if="fieldChanged" :content="'Save changes'" />
       </form>
+
       <div class="delete-button">
         <UIButtonStandard :color="'red'" :icon="IconType.Trashcan" :content="'Delete'" :action="deleteFlexWorker" />
       </div>
